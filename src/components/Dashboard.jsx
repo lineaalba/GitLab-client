b50b5b5de20d38b2175622f826517d07a99fad32
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-26 18:54:53 +0100

    trying to fix cors issue

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -31,6 +31,7 @@ export default class Dashboard extends Component {
             headers:{
                 'Content-Type': 'application/json'
               },
+            mode: 'no-cors',
             credentials: 'same-origin',
         })
             .then(res => res.json())
