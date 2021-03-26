0865c83a3040476241fd04f93816c071b1bbeeb2
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-26 22:00:21 +0100

    trying to fix cors issue

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,12 +28,14 @@ export default class Dashboard extends Component {
            
     componentDidMount() {
         fetch(`https://protected-depths-73018.herokuapp.com/groups`, {
-            headers:{
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Credentials": true,
-                "Access-Control-Allow-Origin": '*',
-                credentials: 'true',
-              }
+            credentials: 'include',
+            mode: 'cors'
+            // headers:{
+            //     'Content-Type': 'application/json',
+            //     "Access-Control-Allow-Credentials": true,
+            //     "Access-Control-Allow-Origin": '*',
+            //     credentials: 'true',
+            //   }
         })
             .then(res => res.json())
             .then(json => this.setState({ data: json }))
