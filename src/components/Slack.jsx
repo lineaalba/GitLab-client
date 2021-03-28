7d1b39cbbf43d17cf6702ac6f91fbf20f4d90c89
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-03-28 11:34:42 +0200

    debugging

--- src/components/Slack.jsx
+++ src/components/Slack.jsx
@@ -20,11 +20,11 @@
     handleChange = (e) => {
         e.preventDefault()
         this.setState({input: e.target.value})
+        console.log(this.state.input)
     }
 
     async onSubmit(e) {
         e.preventDefault()
-        console.log(this.state.input)
         await fetch('https://protected-depths-73018.herokuapp.com/slack', {
                     method: 'POST',
                     credentials: 'include',
